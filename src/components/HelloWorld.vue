130fb7fd91e003ed15eab5ba75f618a76e35d1ea
Kite <ixkaito@gmail.com>
2018-06-07 17:08:54 +0900

    Add table

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -20,32 +20,6 @@
         <td></td>
       </tr>
     </table>
-    <h1>test</h1>
-    <p>
-      For guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://github.com/vuejs/vue-cli/tree/dev/docs" target="_blank">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
   </div>
 </template>
 
@@ -74,5 +48,24 @@ li {
 }
 a {
   color: #42b983;
+}
+
+table {
+  border-collapse: collapse;
+  margin: auto;
+  max-width: 640px;
+}
+
+thead {
+
+  th, td {
+    border-bottom: 2px solid #e6e9ed;
+  }
+}
+
+th, td {
+  border-bottom: 1px solid #e6e9ed;
+  min-width: 150px;
+  padding: 0.5em 1em;
 }
 </style>
